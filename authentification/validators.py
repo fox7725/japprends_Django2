be5eacec117b4e17aa11e0c4e6382904be678985
Christophe <87915060+fox7725@users.noreply.github.com>
2024-03-07 15:52:55 +0100

    ajout du changement de photo de profil

--- authentification/validators.py
+++ authentification/validators.py
@@ -8,3 +8,13 @@ class ContainsLetterValidator:
 
     def get_help_text(self):
         return 'le mot de passe doit contenir au moins une lettre majuscule ou minuscule'
+
+
+class ContainsNumberValidator:
+    def validate(self, password, user=None):
+        if not any(char.isdigit() for char in password):
+            raise ValidationError(
+                'Le mot de passe doit contenir un chiffre', code='password_no_number')
+
+    def get_help_text(self):
+        return 'Votre mot de passe doit contenir au moins un chiffre.'
--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -1,21 +1,6 @@
-"""
-URL configuration for djangoProject2 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import (
+    LoginView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
 
 import authentification.views
@@ -28,10 +13,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LoginView.as_view(
         template_name='authentification/login.html',
-        redirect_authenticated_user=True
-    ), name='login'),
+        redirect_authenticated_user=True),
+         name='login'),
     path('logout/', authentification.views.logout_user, name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name='authentification/password_change_form.html'),
+         name='password_change'
+    ),
+    path('change-password-done/', PasswordChangeDoneView.as_view(
+        template_name='authentification/password_change_done.html'),
+    ),
     path('signup', authentification.views.signup_page, name='signup'),
+    path('profile-photo/upload', authentification.views.upload_profile_photo,
+         name='upload_profile_photo'),
     path('home/', blog.views.home, name='home'),
     path('photo/upload/', blog.views.photo_upload, name='photo_upload')
 ]
